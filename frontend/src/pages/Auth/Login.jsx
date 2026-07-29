4ec4203974d9bd824436e09352b759eb90cf51a0
B O <Oviya.Baskaran@copperhawk.tech>
2026-07-29 12:21:10 +0530

    Fix Logo component casing for Vercel build

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -11,7 +11,7 @@ import {
 import { login } from "../../services/authService";
 import { loginSuccess } from "../../store/authSlice";
 
-import Logo from "../../components/common/Logo";
+import Logo from "../../components/common/logo";
 import Button from "../../components/ui/Button";
 import Input from "../../components/ui/Input";
 import Card from "../../components/ui/Card";
